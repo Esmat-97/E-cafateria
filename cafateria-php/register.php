add7462ccc1834d80efae1473942d13c1dfe67e0
mohamedesmat1997 <158211063+Esmat-97@users.noreply.github.com>
2024-03-18 02:27:12 +0200

    register validation

--- cafateria-php/register.php
+++ cafateria-php/register.php
@@ -15,6 +15,7 @@
     <p><input type="text" name="lastName" placeholder="lastName"></p>
     <p><input type="email" name="email" placeholder="email"></p>
     <p><input type="text" name="password" placeholder="password"></p>
+    <p><input type="text" name="role" placeholder="role"></p>
     <input type="submit" name="submit" value="submit">
 </form>
 
@@ -31,22 +32,34 @@ if(isset($_POST['submit'])){
     $lastName=$_POST['lastName'];
     $email=$_POST['email'];
     $pass=$_POST['password'];
+    $role=$_POST['role'];
     $reg_date = date("Y-m-d H:i:s");
 
 
+
+
+echo empty($_POST['firstName']) ? "firstName is required<br>" : (preg_match('/[a-zA-Z]/', $_POST['firstName']) ? "firstName is valid<br>" : "firstName is not valid<br>");
+echo empty($_POST['lastName']) ? "lastName is required<br>" : (preg_match('/[a-zA-Z]/', $_POST['lastName']) ? "lastName is valid<br>" : "lastName is not valid<br>");
+echo empty($_POST['email']) ? "email is required<br>" : (preg_match('/[a-zA-Z]/', $_POST['email']) ? "email is valid<br>" : "email is not valid<br>");
+echo empty($_POST['role']) ? "role is required<br>" : (preg_match('/[a-zA-Z]/', $_POST['role']) ? "role is valid<br>" : "role is not valid<br>");
+echo empty($_POST['password']) ? "password is required<br>" : 
+    (!preg_match("/[a-z]/",$_POST['password']) ? "password must have lower<br>" : "password valid lower<br>") .
+    (!preg_match("/[A-Z]/",$_POST['password']) ? "password must have upper<br>" : "password valid upper<br>") .
+    (!preg_match("/[0-9]/",$_POST['password']) ? "password must have numbers<br>" : "password valid number<br>") .
+    (!preg_match("/[!@#$%^&*()-_=+{};:,<.>]/",$_POST['password']) ? "password must have special characters<br>" : "password valid special characters<br>").
+
+
+
+   
 
     $servername = "localhost";
     $username = "root";
     $password = "";
     $dbname="cafateria_php";
 
-    // Create connection
     $conn = mysqli_connect($servername, $username, $password , $dbname);
-
-    // Check connection
     if (!$conn) {
         echo "Error: Unable to connect to MySQL." ;
-    
     }
     echo "Connected successfully";
 
@@ -55,8 +68,8 @@ if(isset($_POST['submit'])){
 
 
 
-    $sql="INSERT INTO MyGuests (firstName, lastName, email, password ,reg_date)
-    VALUES ('$firstName', '$lastName', '$email', '$pass','$reg_date')";
+    $sql="INSERT INTO MyGuests (firstName, lastName, email, password ,role,reg_date)
+    VALUES ('$firstName', '$lastName', '$email', '$pass', '$role','$reg_date')";
     
     if (mysqli_query($conn, $sql)) {
       echo "New record created successfully";
