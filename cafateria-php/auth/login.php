997e169a03a6cf769c22749715169ee288ef4531
mohamedesmat1997 <158211063+Esmat-97@users.noreply.github.com>
2024-03-18 17:37:34 +0200

    pages,auth

--- cafateria-php/auth/login.php
+++ cafateria-php/auth/login.php
@@ -4,44 +4,62 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
- <link rel="stylesheet" href="css/register.css">
+    <link rel="stylesheet" href="register.css">
+<style>
+    a{
+    display: inline-block;
+    padding: 10px 20px;
+    background-color: #4CAF50; /* Green */
+    color: white;
+    text-align: center;
+    text-decoration: none;
+    font-size: 16px;
+    border-radius: 5px;
+    border: none;
+    cursor: pointer;
+    margin: 10px;
+  }
+  
+  a:hover{
+    background-color: #45a049; /* Darker green */
+  }
+</style>
 </head>
 <body>
     
 <div class="form">
 
+<a href="login.php">Login</a>
+<a href="register.php">Register</a>
+
+
 <form action="" method="post">
-    <p><input type="text" name="firstName" placeholder="firstName"></p>
-    <p><input type="text" name="lastName" placeholder="lastName"></p>
+
     <p><input type="email" name="email" placeholder="email"></p>
     <p><input type="text" name="password" placeholder="password"></p>
-    <p><input type="text" name="role" placeholder="role"></p>
     <input type="submit" name="submit" value="submit">
+
 </form>
+
 
 </div>
 
+
 </body>
+
 </html>
 
 <?php
 
 if(isset($_POST['submit'])){
  
-    $firstName=$_POST['firstName'];
-    $lastName=$_POST['lastName'];
+
     $email=$_POST['email'];
     $pass=$_POST['password'];
-    $role=$_POST['role'];
-    $reg_date = date("Y-m-d H:i:s");
 
 
 
-
-echo empty($_POST['firstName']) ? "firstName is required<br>" : (preg_match('/[a-zA-Z]/', $_POST['firstName']) ? "firstName is valid<br>" : "firstName is not valid<br>");
-echo empty($_POST['lastName']) ? "lastName is required<br>" : (preg_match('/[a-zA-Z]/', $_POST['lastName']) ? "lastName is valid<br>" : "lastName is not valid<br>");
 echo empty($_POST['email']) ? "email is required<br>" : (preg_match('/[a-zA-Z]/', $_POST['email']) ? "email is valid<br>" : "email is not valid<br>");
-echo empty($_POST['role']) ? "role is required<br>" : (preg_match('/[a-zA-Z]/', $_POST['role']) ? "role is valid<br>" : "role is not valid<br>");
 echo empty($_POST['password']) ? "password is required<br>" : 
     (!preg_match("/[a-z]/",$_POST['password']) ? "password must have lower<br>" : "password valid lower<br>") .
     (!preg_match("/[A-Z]/",$_POST['password']) ? "password must have upper<br>" : "password valid upper<br>") .
@@ -66,17 +84,32 @@ echo empty($_POST['password']) ? "password is required<br>" :
 
     
 
+    $sql="SELECT * FROM MyGuests WHERE email='$email' AND  password='$pass' ";
+    $data=mysqli_query($conn,$sql);
 
-
-    $sql="INSERT INTO MyGuests (firstName, lastName, email, password ,role,reg_date)
-    VALUES ('$firstName', '$lastName', '$email', '$pass', '$role','$reg_date')";
-    
-    if (mysqli_query($conn, $sql)) {
-      echo "New record created successfully";
+    if ($data) {
+      echo "getting data successfully";
     } else {
       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
     
-    mysqli_close($conn);
+
+
+
+    if(mysqli_num_rows($data) > 0){
+
+   $fetcheddata=mysqli_fetch_array($data);
+
+   setcookie('username',$fetcheddata['email'],time() + 60*60);
+
+   header("location:base.php");
+}else{
+   echo "fail coonection";
 }
+    }
+
+   
+
+    mysqli_close($conn);
+
 ?>
---
+++ cafateria-php/auth/logout.php
@@ -0,0 +1,4 @@
+<?php
+setcookie('username','',time()-60*60);
+header("location:register.php")
+?>
